abeca15783e09d49ee934edc7233c8890d28c3b1
Maharta <gedemaharta.dev@gmail.com>
2022-04-18 20:43:42 +0800

    adjusted nav links for mobile to not focusable when it is not showing on the screen.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,15 +6,26 @@ import "../styles/responsive.css";
 import "./restaurant-items.js";
 
 const navList = document.querySelector(".nav_list");
+const navListLinks = navList.querySelectorAll("a");
 const burgerButton = document.querySelector(".hamburger");
 
 const openDrawer = (event) => {
   navList.classList.toggle("show");
+  if (navList.classList.contains("show")) {
+    navListLinks.forEach((link) => {
+      link.setAttribute("tabindex", "0");
+    });
+  } else {
+    navListLinks.forEach((link) => {
+      link.setAttribute("tabindex", "-1");
+    });
+  }
   event.stopPropagation();
 };
 
 burgerButton.addEventListener("click", openDrawer);
 
 document.body.addEventListener("click", () => {
+  navList.setAttribute("tabindex", "-1");
   navList.classList.remove("show");
 });
